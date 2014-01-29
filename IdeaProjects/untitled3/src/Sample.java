82edf8407de86c843134e62336a43900d3383765
rbarry825 <rbarry825@gmail.com>
2014-01-29 17:18:18 -0500

    First update

---
+++ IdeaProjects/untitled3/src/Sample.java
@@ -0,0 +1,10 @@
+/**
+ * @author: Levi
+ * @version: 1/29/14
+ */
+public class Sample {
+    static void main (String [] args){
+        System.out.print("Hello World!");
+        System.out.print("Goodbye");
+    }
+}
